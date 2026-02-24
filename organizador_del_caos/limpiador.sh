cbbd37a826c9e748632c7f4f1ccc5dda81e0ec99
jp <juanpdam2801@gmail.com>
2026-02-24 20:30:29 +0000

    script

--- organizador_del_caos/limpiador.sh
+++ organizador_del_caos/limpiador.sh
@@ -1,8 +1,53 @@
+imgs=0
+pdfs=0
+docs=0
+txts=0
+empty=0
 
-mkdir imgs pdfs docs txts vacios;
-for file in organizador_del_caos/*;do
-[ -e "$file" ] || continue
-find . -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.gif" \) -exec mv -t /organizador_del_caos/imgs/ ;
+for file in /organizador_del_caos/*; do
+if ls  *.txt >/dev/null 2>1; then
+echo "si existen txts, moviendo archivos"
+imgs++
+else
+echo "no existe"
+fi
+if ls *.jpg >/dev/null  2>1; then
+echo "si existen imgs,moviendo archivos"
+imgs++
+else
+echo "no existe"
+fi
+if ls *.png >/dev/null 2>1; then
+echo "si existen pngs, moviendo archivos"
+imgs++
+else
+echo "no existen"
+fi
+if ls *.gif >/dev/null 2>1; then
+echo "si existen gifs,moviendo archivos"
+imgs++
+else
+echo "no existen"
+fi
+if ls *.pdf >/dev/null 2>1; then
+echo "si existen pdfs, moviendo archivos"
+pdfs++
+else
+echo "no existen"
+fi
+if ls *.docx >/dev/null 2>1; then
+echo "si existen docxs, moviendo archivos"
+docs++
+else
+echo "no existen"
+fi
+if ls *.odt >/dev null/ 2>1; then
+echo "si existen odt, moviendo archivos"
+docs++
+else
+echo "no existen"
+fi
+echo "$imgs"
 done
 
 
